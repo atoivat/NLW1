42fd42fbc72b6470e51d933dba6bbcd1cf08226a
Otavio <otavio.leite.ufu@gmail.com>
2020-06-04 11:04:43 -0300

    Aula 4

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const server = express();
+
+// Pasta public
+server.use(express.static("public"));
+
+// Template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true,
+});
+
+
+// Configura caminhos
+// Pagina inicial
+// req >> requisicao
+// res >> resposta
+server.get("/", (req, res) => {
+    return res.render("index.html");
+})
+
+// Create point
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html");
+})
+
+// Search results
+server.get("/search", (req, res) => {
+    return res.render("search-results.html");
+})
+
+// Ligar o servidor
+server.listen(3000);
